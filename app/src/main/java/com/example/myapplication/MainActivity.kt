87a7f3d3fdd76f49e6a9b0e86a45dbf324939902
lurenjia534 <lurenjia534@outlook.com>
2023-11-08 22:33:56 +0800

    App: An icon for the future of about interfaces

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -13,9 +13,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.twotone.Favorite
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Surface
@@ -49,7 +53,7 @@ class MainActivity : ComponentActivity() {
 
     @OptIn(ExperimentalMaterial3Api::class)
     @Composable
-    fun AppUI(color:Color) {
+    fun AppUI(color: Color) {
         MaterialTheme {
             Surface {
                 val systemUiController = rememberSystemUiController()
@@ -72,6 +76,19 @@ class MainActivity : ComponentActivity() {
                             )
                         },
                         colors = TopAppBarDefaults.centerAlignedTopAppBarColors(containerColor = MaterialTheme.colorScheme.primaryContainer),
+                        navigationIcon = {
+                            IconButton(onClick = { /*TODO*/ }) {
+
+                            }
+                        },
+                        actions = {
+                            IconButton(onClick = { /*TODO*/ }) {
+                                Icon(
+                                    imageVector = Icons.TwoTone.Favorite,
+                                    contentDescription = null,
+                                )
+                            }
+                        }
                     )
 
                     val androidVersion = Build.VERSION.RELEASE // Android版本
