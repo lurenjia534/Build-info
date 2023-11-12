7eaa75f13f068c044ae83592d4e968e6536480eb
lurenjia534 <lurenjia534@outlook.com>
2023-11-12 23:30:34 +0800

    App: Compromise! Finally we adapted minSDK 31 Android S

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.lurenjia534.Buildinfo"
-        minSdk = 26
+        minSdk = 31
         targetSdk = 34
         versionCode = 1
         versionName = "1.1"
