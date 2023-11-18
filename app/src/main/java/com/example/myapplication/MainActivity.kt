05ca6d2dc9688ce89b6934fd57bbbc718921e697
lurenjia534 <lurenjia534@outlook.com>
2023-11-18 16:22:19 +0800

    App: Multi-language support

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -48,6 +48,7 @@ import androidx.compose.ui.platform.ClipboardManager
 import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
@@ -92,7 +93,7 @@ class MainActivity : ComponentActivity() {
                     CenterAlignedTopAppBar(
                         title = {
                             Text(
-                                text = "Device info",
+                                text = stringResource(id = R.string.Device_info),
                                 color = MaterialTheme.colorScheme.onSurface,
                                 style = MaterialTheme.typography.titleLarge
                             )
